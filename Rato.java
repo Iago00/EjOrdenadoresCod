b5d8da52d1123e242ef0b84a02742fdb11fca92a
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2019-11-27 13:41:09 +0100

    cod

---
+++ Rato.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Rato {
+     public String tipo;
+    public Rato(){
+        
+    }
+    /**
+     * se crea constructor con parametros y se le añade el tipo
+     * @param tipo 
+     */
+    public Rato(String tipo){
+        this.tipo=tipo;
+    }
+    /**
+     * 
+     * @param tipo muestra el tipo string
+     */
+    public void setTipo(String tipo){
+        this.tipo=tipo;
+    }
+    /**
+     * 
+     * @return devuelve el tipo string
+     */
+    public String getTipo(){
+        return tipo;
+    }
+}
